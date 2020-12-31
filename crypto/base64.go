bfee0c6e33372e84cd115d1067ba63f8fd3cbf08
litk <support@getssl.cn>
2020-12-31 11:51:51 +0800

    Inital

---
+++ crypto/base64.go
@@ -0,0 +1,15 @@
+package crypto
+
+import "encoding/base64"
+
+func Encode(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func Decode(src string) string {
+	data, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
